765690934042030ff09ae2f5520de2f2adea9100
luo-yuyuan <34554308+luo-yuyuan@users.noreply.github.com>
2017-12-17 17:24:04 -0500

    Update StoreActivity.java

--- PowerUp/app/src/main/java/powerup/systers/com/StoreActivity.java
+++ PowerUp/app/src/main/java/powerup/systers/com/StoreActivity.java
@@ -61,6 +61,7 @@ public class StoreActivity extends AppCompatActivity {
             public void onClick(View v) {
                 finish();
                 startActivity(new Intent(StoreActivity.this,MapActivity.class));
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
             }
         });
 
